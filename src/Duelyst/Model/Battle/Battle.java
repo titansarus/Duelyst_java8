31f5645e7cc103127224f3ea37ad1da9bd4de764
Amirmahdi Namjoo <amirm137878@gmail.com>
2019-06-07 14:19:44 +0430

    Battle some initial methods done.

---
+++ src/Duelyst/Model/Battle/Battle.java
@@ -0,0 +1,82 @@
+package Duelyst.Model.Battle;
+
+import Duelyst.Model.Account;
+
+import static Duelyst.View.Constants.*;
+
+public class Battle {
+    private Player player1;
+    private Player player2;
+    private Player playingPlayer = null;
+    private Cell[][] grid = new Cell[BATTLE_ROWS][BATTLE_COLUMNS];
+    private int turn = 1;
+
+    public void initializeCells() {
+        for (int i = 0; i < BATTLE_ROWS; i++) {
+            for (int j = 0; j < BATTLE_COLUMNS; j++) {
+                getGrid()[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    public Battle(Account account1, Account account2) {
+        setPlayer1(new Player(account1, account1.getCardCollection().getMainDeck()));
+        setPlayer2(new Player(account2, account2.getCardCollection().getMainDeck()));
+        setPlayingPlayer();
+        initializeCells();
+    }
+
+    public void nextTurn()
+    {
+        turn++;
+        setPlayingPlayer();
+        getPlayer1().setManaFromTurn(getTurn());
+        getPlayer2().setManaFromTurn(getTurn());
+
+    }
+
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Cell[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Cell[][] grid) {
+        this.grid = grid;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    public Player getPlayingPlayer() {
+       return playingPlayer;
+    }
+
+    public void setPlayingPlayer() {
+        if (turn % 2 == 1) {
+            this.playingPlayer = player1;
+            return;
+        }
+        this.playingPlayer= player2;
+    }
+}
---
+++ src/Duelyst/Model/Battle/Cell.java
@@ -0,0 +1,37 @@
+package Duelyst.Model.Battle;
+
+import Duelyst.Model.Warrior;
+
+public class Cell {
+    private Integer row , column;
+    private Warrior warrior;
+
+    public Cell(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public Warrior getWarrior() {
+        return warrior;
+    }
+
+    public void setWarrior(Warrior warrior) {
+        this.warrior = warrior;
+    }
+}
