cc5265144b7cb07c3b7d149cb94cafb5faacd465
rezaaminimajd <45604304+rezaaminimajd@users.noreply.github.com>
2019-06-24 20:58:32 +0430

    some bug revaled

--- src/Duelyst/Model/Ai.java
+++ src/Duelyst/Model/Ai.java
@@ -6,11 +6,11 @@ public class Ai extends Account {
     public Ai(int numberOfAi) {
         super("AI:)","AI:)");
         if (numberOfAi == 1) {
-            this.setMainDeck(Deck.AiDeckBuilder(1));
+            this.setMainDeck(Deck.AiDeckBuilder(1,this));
         } else if (numberOfAi == 2) {
-            this.setMainDeck(Deck.AiDeckBuilder(2));
+            this.setMainDeck(Deck.AiDeckBuilder(2,this));
         } else if (numberOfAi == 3) {
-            this.setMainDeck(Deck.AiDeckBuilder(3));
+            this.setMainDeck(Deck.AiDeckBuilder(3,this));
         }
         setDarick(1000000000);
     }
