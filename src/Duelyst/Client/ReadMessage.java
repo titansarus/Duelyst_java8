de385f74f3c7cd01f4a4dd708624e1bb6477bfeb
komeilyahyazade <45604304+rezaaminimajd@users.noreply.github.com>
2019-07-03 12:21:23 +0430

    shop cards revaled

--- src/Duelyst/Client/ReadMessage.java
+++ src/Duelyst/Client/ReadMessage.java
@@ -4,14 +4,18 @@ import Duelyst.Controllers.Container;
 import Duelyst.Controllers.LoginController;
 import Duelyst.Exceptions.UserNotExistException;
 import Duelyst.Model.Account;
+import Duelyst.Model.Card;
 import Duelyst.Model.CommandClasses.CommandClass;
 import Duelyst.Model.CommandClasses.LoginCommand;
+import Duelyst.Model.CommandClasses.ShopCommand;
+import Duelyst.Model.Shop;
 import com.gilecode.yagson.YaGson;
 import com.gilecode.yagson.YaGsonBuilder;
 
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadMessage extends Thread {
@@ -19,7 +23,7 @@ public class ReadMessage extends Thread {
     private Scanner netIn;
     private CommandClass commandClass;
 
-    public ReadMessage(Socket socket){
+    public ReadMessage(Socket socket) {
         try {
             this.netIn = new Scanner(socket.getInputStream());
         } catch (IOException e) {
@@ -38,18 +42,17 @@ public class ReadMessage extends Thread {
             System.out.println("In reader");
             switch (commandClass.getCommandKind()) {
                 case LOGIN:
-                    LoginCommand loginCommand = (LoginCommand)commandClass;
-                    if(loginCommand.getMyException() != null) {
+                    LoginCommand loginCommand = (LoginCommand) commandClass;
+                    if (loginCommand.getMyException() != null) {
                         LoginController.setMyException(loginCommand.getMyException());
                         System.out.println("Have Exception");
-                    }
-                    else {
+                    } else {
                         Account.setLoggedAccount(loginCommand.getAccount());
                         System.out.println("Account LoggedIn!");
                     }
                     break;
                 case SHOP:
-                    //TODO Bayad liste card haye shope server ra begirad
+                    handleShopCommand((ShopCommand)commandClass);
                     break;
                 case BATTLE:
                     //TODO Bayad Command Haye Marboot be battle ra begirad
@@ -57,7 +60,21 @@ public class ReadMessage extends Thread {
             }
 
         }
+    }
 
+    private void handleShopCommand(ShopCommand shopCommand) {
+        switch (shopCommand.getShopCommandsKind()) {
+            case AUCTION_CARD:
+                break;
+            case SEND_CARD:
+                break;
+            case GET_CARDS:
+                handleGetCards(shopCommand);
 
+        }
+    }
+    private void handleGetCards(ShopCommand shopCommand){
+        ArrayList<Card> cards = shopCommand.getCards();
+        Shop.getInstance().getCards().addAll(cards);
     }
 }
